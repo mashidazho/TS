db959d4f6335e36d0c6d862a330123aa6e7184fc
mashidazho <150504902+mashidazho@users.noreply.github.com>
2024-06-14 14:54:37 +1000

    Add files via upload

---
+++ zxc.ts
@@ -0,0 +1,27 @@
+class Point {
+    x9?: number 
+    y9?: number
+    constructor(floor1?: number, floor2?: number) {
+        this.x9 = floor1
+        this.y9 = floor2
+    }
+}
+
+class Triangle {
+        x8?: Point
+        y8?: Point
+        z8?: Point
+    constructor(x1?: number, y1?: number,x2?: number, y2?: number,x3?: number, y3?: number) {
+        this.x8 = new Point(x1, y1);
+        this.y8 = new Point(x2, y2);
+        this.z8 = new Point(x3, y3);
+
+    }
+}
+
+let point1: Point = new Point(5,6)
+let triangle1 = new Triangle()
+let triangle2 = new Triangle(1,2,3,4,5,6)
+
+console.log(triangle1)
+console.log(triangle2)
